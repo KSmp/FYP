bc96e1da1995bd489a6b290ebb98f21434103014
KSmp <kacper@sampolski.com>
2022-04-21 03:56:55 +0100

    Project base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './main/main.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { ContentComponent } from './content/content.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+
+const material = [
+  MatSidenavModule,
+  MatToolbarModule,
+  MatIconModule,
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SideBarComponent,
+    ContentComponent,
+    ToolbarComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ...material,
   ],
   providers: [],
   bootstrap: [AppComponent]
